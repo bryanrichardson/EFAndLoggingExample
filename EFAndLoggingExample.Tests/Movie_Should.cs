714705e2a9242d92b0e4ffff48c436a27f78eca7
Bryan Richardson <richardson.bryan@gmail.com>
2014-06-06 20:20:49 -0500

    Movie model tests complete

--- EFAndLoggingExample.Tests/Movie_Should.cs
+++ EFAndLoggingExample.Tests/Movie_Should.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data.Entity;
 using System.Linq;
+using System.Threading;
 using EFAndLoggingExample.Models;
 using NUnit.Framework;
 
@@ -26,6 +27,66 @@ namespace EFAndLoggingExample.Tests
             var query = db.Movies.Where(m => m.Name == "Star Wars");
 
             Assert.AreEqual(1,query.Count());
+        }
+
+        [Test]
+        public void Update_A_Movie()
+        {
+            // Create movie to update
+            var db = new MovieContext();
+
+            var movie = new Movie() { Name = "Star Wars" };
+            db.Movies.Add(movie);
+            db.SaveChanges();
+
+            // Update movie
+            var movieToChange = db.Movies.First(m => m.Name == "Star Wars");
+            movieToChange.Name = "Lord of the Rings";
+            db.SaveChanges();
+            
+            // Setup Query
+            var query = db.Movies.Where(m => m.Name == "Lord of the Rings");
+
+            // Test return values
+            Assert.AreEqual(1, query.Count());
+        }
+
+        [Test]
+        public void Delete_A_Movie()
+        {
+            using (var db = new MovieContext())
+            {
+                // Create movie to delete
+                var movie = new Movie() {Name = "Star Wars"};
+                db.Movies.Add(movie);
+                db.SaveChanges();
+
+                // Delete movie
+                var movieToDelete = db.Movies.First(m => m.Name == "Star Wars");
+                db.Movies.Remove(movieToDelete);
+                db.SaveChanges();
+                
+                // Test return values
+                Assert.AreEqual(EntityState.Detached, db.Entry(movieToDelete).State);
+            }
+        }
+
+        [Test]
+        public void Retrieve_A_Movie()
+        {
+            using (var db = new MovieContext())
+            {
+                // Create movie to retrieve
+                var movie = new Movie() { Name = "Star Wars" };
+                db.Movies.Add(movie);
+                db.SaveChanges();
+
+                // Retrieve movie
+                var movieToDelete = db.Movies.First(m => m.Name == "Star Wars");
+
+                // Test return values
+                Assert.AreEqual("Star Wars", movieToDelete.Name);
+            }
         }
     }
 }
